bd845698d06d3955d73758a9c84276e544a1cdcc
Timoh97 <timohmugendi@gmail.com>
2022-05-13 23:50:53 +0300

    create login function in auth.views

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,13 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 @main.route('/')
 def index():
     return render_template("index.html")
 
 @main.route('/cart')
+@login_required
 def cart():
     return render_template("cart.html")
 
@@ -15,6 +17,7 @@ def about():
     return render_template("about.html")
 
 @main.route('/checkout')
+
 def checkout():
 
     return render_template("checkout.html")
@@ -28,18 +31,22 @@ def indexx():
     return render_template("index_2.html")
 
 @main.route('/news')
+@login_required
 def news():
 
     return render_template("news.html")
 
 @main.route('/shop')
+@login_required
 def shop():
     return render_template("shop.html")
 
 @main.route('/singles')
+@login_required
 def single():
     return render_template("single-news.html")
 
 @main.route('/product')
+@login_required
 def product():
     return render_template("single-product.html") 
