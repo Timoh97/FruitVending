4ed515150ebb39dc557767ca9ce33af651867946
Timoh97 <timohmugendi@gmail.com>
2022-05-13 13:43:30 +0300

    set navbar in fruit vendor

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,6 @@ from . import main
 
 @main.route('/')
 def index():
-    title=''
-    subject='applicationdevelopment'
-    return render_template("index.html",name=title,subject=subject)
+    title='OnlineFruits'
+    subject='application development'
+    return render_template("index.html")
--- config.py
+++ config.py
@@ -1,18 +1,22 @@
-
 import os
 
+#s1st step set configurations
 
-class Config():
-    SECRET_KEY='TIM'
-class DevConfig():
-     
+class Config:
+
+   
+    SECRET_KEY = 'tim'
+   
+
+class ProdConfig(Config):
     pass
-    
-class ProdConfig():
-    Debug=True
-    
-config_options={
-    'development':DevConfig,
-    'production':ProdConfig
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
 }
-    
+
